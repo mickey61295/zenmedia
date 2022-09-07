f9a052ea0ca76b6565c5646b35f3b4512b700983
mickey61295 <31827390+mickey61295@users.noreply.github.com>
2022-09-07 11:55:54 +0530

    Update Auth.jsx

--- frontend/src/pages/Auth/Auth.jsx
+++ frontend/src/pages/Auth/Auth.jsx
@@ -56,8 +56,8 @@ const Auth = () => {
         <img src={Logo} alt="" />
 
         <div className="Webname">
-          <h1>ZKC Media</h1>
-          <h6>Explore the ideas throughout the world</h6>
+          <h1>ZEN Media</h1>
+          <h6>Built to showcase skills</h6>
         </div>
       </div>
 
